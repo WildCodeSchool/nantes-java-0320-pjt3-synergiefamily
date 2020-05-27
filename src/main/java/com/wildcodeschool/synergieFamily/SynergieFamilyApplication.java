534a4c0650cc1b5f7097044d91455970535edb77
bastienwcs <bastien@wldcodeschool.fr>
2020-05-27 15:50:37 +0200

    init

---
+++ src/main/java/com/wildcodeschool/synergieFamily/SynergieFamilyApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.synergieFamily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynergieFamilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynergieFamilyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/synergieFamily/SynergieFamilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.synergieFamily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynergieFamilyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
