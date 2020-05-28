94b485ac42892d2743472609068c5e16139c6fc7
Alanrl22 <alan.raoul22@gmail.com>
2020-05-28 17:20:30 +0200

    Creation of the entity Skill and SkillRepository

--- src/main/java/com/wildcodeschool/synergieFamily/repository/SkillRepository.java
+++ src/main/java/com/wildcodeschool/synergieFamily/repository/SkillRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SkillRepository extends JpaRepository<Skill, Long> {
-}
+public interface SkillRepository extends JpaRepository<Skill, Long> {}
