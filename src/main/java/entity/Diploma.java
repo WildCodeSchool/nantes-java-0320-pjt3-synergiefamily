c705cf256206e8c92193c9b790224e375367ac54
AnnCa44 <ann.cariou@gmail.com>
2020-05-28 11:29:20 +0200

    create entity diploma

---
+++ src/main/java/entity/Diploma.java
@@ -0,0 +1,33 @@
+package entity;
+
+public class Diploma {
+
+    private int id;
+    private String name;
+
+    public Diploma(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Diploma() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+
