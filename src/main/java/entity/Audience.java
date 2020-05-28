445c94b5436b0697cd0c348abfe1083674a7c8c1
Alanrl22 <alan.raoul22@gmail.com>
2020-05-28 11:43:30 +0200

    Creation of the entity Audience

--- src/main/java/entity/Audience.java
+++ src/main/java/entity/Audience.java
@@ -1,4 +1,18 @@
 package entity;
 
 public class Audience {
+
+    private String age_bracket;
+
+    public Audience(String age_bracket) {
+        this.age_bracket = age_bracket;
+    }
+
+    public String getAge_bracket() {
+        return age_bracket;
+    }
+
+    public void setAge_bracket(String age_bracket) {
+        this.age_bracket = age_bracket;
+    }
 }
