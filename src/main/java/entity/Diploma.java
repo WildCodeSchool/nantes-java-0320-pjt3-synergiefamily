529ef61ba4f7ffb75d5144024c5cc5ad184c708f
AnnCa44 <ann.cariou@gmail.com>
2020-05-28 12:09:01 +0200

    add @Id and @GeneratedValue

--- src/main/java/entity/Diploma.java
+++ src/main/java/entity/Diploma.java
@@ -1,10 +1,15 @@
 package entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Diploma {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
 
