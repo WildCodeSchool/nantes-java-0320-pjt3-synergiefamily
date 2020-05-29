662a6fb17a4eb1b52da7c17d8ac4e002bca6d0a9
swann <chevreauswann@outlook.fr>
2020-05-29 11:13:24 +0200

    création de l'entity User, de son repository ainsi que de son controller

---
+++ src/main/java/com/wildcodeschool/synergieFamily/entity/User.java
@@ -0,0 +1,54 @@
+package com.wildcodeschool.synergieFamily.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private int idRole;
+
+    public User() {
+
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword() {
+        this.password = password;
+    }
+
+    public int getIdRole() {
+        return this.idRole;
+    }
+
+    public void setIdRole(int idRole) {
+        this.idRole = idRole;
+    }
+}
