6815ee50ab3aa7e882a0bb3d2cb16572393fe67b
Alanrl22 <alan.raoul22@gmail.com>
2020-05-29 07:32:56 +0200

    Creation of the entity Location and LocationRepository

--- src/main/java/com/wildcodeschool/synergieFamily/entity/Location.java
+++ src/main/java/com/wildcodeschool/synergieFamily/entity/Location.java
@@ -12,8 +12,8 @@ public class Location {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String adress1;
-    private String adress2;
+    private String address1;
+    private String address2;
     private int postcode;
     private String city;
 
@@ -27,20 +27,20 @@ public class Location {
         this.id = id;
     }
 
-    public String getAdress1() {
-        return adress1;
+    public String getAddress1() {
+        return address1;
     }
 
-    public void setAdress1(String adress1) {
-        this.adress1 = adress1;
+    public void setAddress1(String address1) {
+        this.address1 = address1;
     }
 
-    public String getAdress2() {
-        return adress2;
+    public String getAddress2() {
+        return address2;
     }
 
-    public void setAdress2(String adress2) {
-        this.adress2 = adress2;
+    public void setAddress2(String adress2) {
+        this.address2 = adress2;
     }
 
     public int getPostcode() {
