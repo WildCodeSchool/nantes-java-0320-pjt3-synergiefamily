aabb8e4cea50bd9739f9caf51906a191612f1366
Alanrl22 <alan.raoul22@gmail.com>
2020-06-02 09:42:38 +0200

    Creation of the filter module

--- src/main/java/com/wildcodeschool/synergieFamily/controller/UserController.java
+++ src/main/java/com/wildcodeschool/synergieFamily/controller/UserController.java
@@ -3,10 +3,18 @@ package com.wildcodeschool.synergieFamily.controller;
 import com.wildcodeschool.synergieFamily.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
     @Autowired
     private UserRepository repository;
+
+    //TODO DEPLACER LA METHODE PLUS TARD VOIR AVEC ALAN
+    @GetMapping("/filter")
+    public String getFilter(){
+        return "filter";
+    }
+
 }
