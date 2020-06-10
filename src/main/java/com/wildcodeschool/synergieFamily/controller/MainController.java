b33732c12141f0fcbf84cb43edc76fbb2f511f23
swann <chevreauswann@outlook.fr>
2020-06-10 16:13:27 +0200

    création page user animator

--- src/main/java/com/wildcodeschool/synergieFamily/controller/MainController.java
+++ src/main/java/com/wildcodeschool/synergieFamily/controller/MainController.java
@@ -31,6 +31,11 @@ public class MainController {
         return "filter";
     }
 
+    @GetMapping("/user-animator")
+    public String getUserAnimator() {
+        return "user-animator";
+    }
+
 
 }
 
