11274e5406969c0cf852be0e762de79ff26083aa
Aurélien Lonni <aurelien.lonni@gmail.com>
2020-06-10 11:59:59 +0200

    Update of UserController

--- src/main/java/com/wildcodeschool/synergieFamily/controller/UserController.java
+++ src/main/java/com/wildcodeschool/synergieFamily/controller/UserController.java
@@ -1,15 +1,24 @@
 package com.wildcodeschool.synergieFamily.controller;
 
+import com.wildcodeschool.synergieFamily.entity.User;
 import com.wildcodeschool.synergieFamily.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class UserController {
 
     @Autowired
-    private UserRepository repository;
+    private UserRepository userRepository;
+
+    @GetMapping("/users")
+    public List<User> showAllUsers() {
+
+        return userRepository.findAll();
+    }
 
 }
 
