ac1dd5475577a243701b2b2f64ae977b93b4621d
AnnCa44 <ann.cariou@gmail.com>
2020-06-11 11:46:12 +0200

    add @Repository

--- src/main/java/com/wildcodeschool/synergieFamily/repository/ActivityLeaderRepository.java
+++ src/main/java/com/wildcodeschool/synergieFamily/repository/ActivityLeaderRepository.java
@@ -2,6 +2,8 @@ package com.wildcodeschool.synergieFamily.repository;
 
 import com.wildcodeschool.synergieFamily.entity.ActivityLeader;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ActivityLeaderRepository extends JpaRepository<ActivityLeader, Long> {
 }
