de8f5b06d0907ec5e8a2417f94b089815c66ed71
AnnCa44 <ann.cariou@gmail.com>
2020-06-12 11:00:46 +0200

    add button submit + thymeleaf in form

--- src/main/java/com/wildcodeschool/synergieFamily/controller/ActivityLeaderController.java
+++ src/main/java/com/wildcodeschool/synergieFamily/controller/ActivityLeaderController.java
@@ -1,14 +1,32 @@
 package com.wildcodeschool.synergieFamily.controller;
 
+import com.wildcodeschool.synergieFamily.entity.ActivityLeader;
+import com.wildcodeschool.synergieFamily.repository.ActivityLeaderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ActivityLeaderController {
 
-    @GetMapping("/activity-leader-creation")
-    public String getActivityLeaderCreation() {
+    @Autowired
+    private ActivityLeaderRepository activityLeaderRepository;
 
+    @GetMapping("/activity-leader-creation")
+    public String getActivityLeaderCreation(Model out) {
+
+        ActivityLeader activityLeader = new ActivityLeader();
+        out.addAttribute("activityLeader", activityLeader);
         return "activity-leader-creation";
+    }
+
+    @PostMapping("/activity-leader-creation")
+    public String postForm(@ModelAttribute ActivityLeader activityLeader) {
+
+        activityLeaderRepository.save(activityLeader);
+        return "redirect:/activity-leader-creation";
     }
 }
