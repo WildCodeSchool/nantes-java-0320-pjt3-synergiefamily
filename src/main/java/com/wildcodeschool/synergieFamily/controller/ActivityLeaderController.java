77c5734cf3d4f5f0a870a510164b7fa899b39a1e
swann <chevreauswann@outlook.fr>
2020-06-12 11:13:56 +0200

    animator search and display by name and surname

--- src/main/java/com/wildcodeschool/synergieFamily/controller/ActivityLeaderController.java
+++ src/main/java/com/wildcodeschool/synergieFamily/controller/ActivityLeaderController.java
@@ -1,14 +1,45 @@
 package com.wildcodeschool.synergieFamily.controller;
 
+import com.wildcodeschool.synergieFamily.entity.ActivityLeader;
+import com.wildcodeschool.synergieFamily.repository.ActivityLeaderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 public class ActivityLeaderController {
+
+    @Autowired
+    private ActivityLeaderRepository activityLeaderRepository;
 
     @GetMapping("/activity-leader-creation")
     public String getActivityLeaderCreation() {
 
         return "activity-leader-creation";
+    }
+
+    @GetMapping("/activity-leader-management")
+    public String getActivityLeaderManagement(Model out) {
+
+        out.addAttribute("activityLeaders", activityLeaderRepository.findAll());
+
+        return "activity-leader-management";
+    }
+
+    @GetMapping("/activityLeaders")
+    public List<ActivityLeader> showAllActivityLeaders() {
+
+        return activityLeaderRepository.findAll();
+    }
+
+    @GetMapping("/activityLeader")
+    public List<ActivityLeader> showActivityLeadersByLastNameFirstNamePhoneEmail(@PathVariable String lastName,
+                                                                                 @PathVariable String firstName) {
+
+        return activityLeaderRepository.findAllByLastNameContainingAndFirstNameContaining(lastName, firstName);
     }
 }
--- src/main/java/com/wildcodeschool/synergieFamily/repository/ActivityLeaderRepository.java
+++ src/main/java/com/wildcodeschool/synergieFamily/repository/ActivityLeaderRepository.java
@@ -4,6 +4,12 @@ import com.wildcodeschool.synergieFamily.entity.ActivityLeader;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ActivityLeaderRepository extends JpaRepository<ActivityLeader, Long> {
+
+    public List<ActivityLeader> findAllByLastNameContainingAndFirstNameContaining(String lastName, String firstName);
+
+
 }
