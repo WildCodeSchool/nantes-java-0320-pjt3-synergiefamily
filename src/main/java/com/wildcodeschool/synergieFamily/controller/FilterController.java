7eb79815d95d5a4c02ab2ec515d20754b602e668
Alanrl22 <alan.raoul22@gmail.com>
2020-06-17 17:46:38 +0200

    Created Repository Method and built param filter

--- src/main/java/com/wildcodeschool/synergieFamily/controller/FilterController.java
+++ src/main/java/com/wildcodeschool/synergieFamily/controller/FilterController.java
@@ -1,16 +1,38 @@
 package com.wildcodeschool.synergieFamily.controller;
 
 import com.wildcodeschool.synergieFamily.entity.ActivityLeader;
-import com.wildcodeschool.synergieFamily.repository.ActivityLeaderRepository;
+import com.wildcodeschool.synergieFamily.entity.Diploma;
+import com.wildcodeschool.synergieFamily.entity.Value;
+import com.wildcodeschool.synergieFamily.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.logging.Filter;
 
 @Controller
 public class FilterController {
 
+    @Autowired
+    private ActivityLeaderRepository activityLeaderRepository;
+    @Autowired
+    private AudienceRepository audienceRepository;
+    @Autowired
+    private LocationRepository locationRepository;
+    @Autowired
+    private DiplomaRepository diplomaRepository;
+    @Autowired
+    private SkillRepository skillRepository;
+    @Autowired
+    private ValueRepository valueRepository;
+
+    @GetMapping("/tacos")
+    @ResponseBody
+    public String filter(@ModelAttribute ActivityLeader activityLeader){
+        List<ActivityLeader> list = activityLeaderRepository.findAllByFilter(activityLeader.getFirstName(),"tacos");
+        return "ok";
+    }
 }
