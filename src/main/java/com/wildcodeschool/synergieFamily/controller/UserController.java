440d9c40091a9bb984faab77df55c57736ab8314
Aurélien Lonni <aurelien.lonni@gmail.com>
2020-06-30 10:30:47 +0200

    Link creation to reach the user-creation.html page

--- src/main/java/com/wildcodeschool/synergieFamily/controller/UserController.java
+++ src/main/java/com/wildcodeschool/synergieFamily/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -38,6 +39,12 @@ public class UserController {
     public List<User> showAllUsers() {
 
         return userRepository.findAll();
+    }
+
+    @GetMapping("/user-creation")
+    public String getUserCreation() {
+
+        return "user-creation";
     }
 }
 
