a28c0bf0de8702705bd92243ff736fa25786c691
Alanrl22 <alan.raoul22@gmail.com>
2020-07-01 17:57:46 +0200

    Created a road for display card activity leader + no editable card activity leader

--- src/main/java/com/wildcodeschool/synergieFamily/controller/FilterController.java
+++ src/main/java/com/wildcodeschool/synergieFamily/controller/FilterController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import java.util.logging.Filter;
 
 @Controller
@@ -56,5 +57,6 @@ public class FilterController {
         model.addAttribute("activityLeader", new ActivityLeader());
         return "filter";
     }
+
 }
 
