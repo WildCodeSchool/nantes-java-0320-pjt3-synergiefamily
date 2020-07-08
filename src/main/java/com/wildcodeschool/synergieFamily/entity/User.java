08d83eef02d7056d9595d2c9fe696c0b2a2c3db3
Aurélien Lonni <aurelien.lonni@gmail.com>
2020-07-08 09:43:38 +0200

    Addition of a new 'disabled' column in the User entity.

--- src/main/java/com/wildcodeschool/synergieFamily/entity/User.java
+++ src/main/java/com/wildcodeschool/synergieFamily/entity/User.java
@@ -28,6 +28,9 @@ public class User {
                 inverseJoinColumns = @JoinColumn(name = "role_id"))
     private List<Role> roles = new ArrayList<>();
 
+    @Column
+    private Boolean disabled;
+
     public User() {
     }
 
