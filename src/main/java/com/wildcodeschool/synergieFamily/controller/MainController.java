2a48087c2025d9550c3f74622c7c86b522e238c2
swann <chevreauswann@outlook.fr>
2020-07-08 16:22:50 +0200

    Revert "modification password"
    This reverts commit 340576a0687a60697ddfc273e4d117ee7aa48309.

--- src/main/java/com/wildcodeschool/synergieFamily/controller/MainController.java
+++ src/main/java/com/wildcodeschool/synergieFamily/controller/MainController.java
@@ -1,10 +1,17 @@
 package com.wildcodeschool.synergieFamily.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-
+    @GetMapping("/profile")
+    public String getProfile() {
+        return "profile";
+    }
 }
 
