0221ddbda0510c209663fa0481368b295e55774b
Aurélien Lonni <aurelien.lonni@gmail.com>
2020-07-08 16:04:18 +0200

    Deletion of dead code

--- src/main/java/com/wildcodeschool/synergieFamily/controller/UserController.java
+++ src/main/java/com/wildcodeschool/synergieFamily/controller/UserController.java
@@ -114,27 +114,12 @@ public class UserController {
         return "redirect:/user-management";
     }
 
-/*    @GetMapping("/user-management")
-    public String getUserManagement(Model out) {
-
-        out.addAttribute("users", userRepository.findAll()); //TODO replace by another query finding all the active users
-        return "user-management";
-    }*/
-
     @GetMapping("/user-management")
     public String getUserManagement(Model out) {
 
         out.addAttribute("users", userRepository.findAllActiveUsers());
         return "user-management";
     }
-
-
-/*    @GetMapping("/user/delete")
-    public String deleteUser(@RequestParam Long id) {
-
-        userRepository.deleteById(id); //TODO: replace by a new query for disabling
-        return "redirect:/user-management";
-    }*/
 
     @GetMapping("/user/disable")
     public String disableUser(@RequestParam Long id) {
