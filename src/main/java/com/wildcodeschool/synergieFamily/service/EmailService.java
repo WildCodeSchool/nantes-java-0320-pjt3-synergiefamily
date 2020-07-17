8d99991f8d815e9edbb4d7413d65d4877d132f19
Aurélien Lonni <aurelien.lonni@gmail.com>
2020-07-17 12:08:48 +0200

    services code cleaning

--- src/main/java/com/wildcodeschool/synergieFamily/service/EmailService.java
+++ src/main/java/com/wildcodeschool/synergieFamily/service/EmailService.java
@@ -16,46 +16,37 @@ public class EmailService {
 
         SimpleMailMessage msg = new SimpleMailMessage();
         msg.setTo(email);
-
         msg.setSubject("Inscription à Synergie Family");
         msg.setText("Bonjour,\n Votre mot de passe est : " + password);
-
         javaMailSender.send(msg);
-
     }
 
-    public void sendNewActivityLeader(String url, ActivityLeader activityLeader, String token){
+    public void sendNewActivityLeader(String url, ActivityLeader activityLeader, String token) {
 
         SimpleMailMessage msg = new SimpleMailMessage();
         msg.setTo(activityLeader.getEmail());
-
         msg.setSubject("Valider votre fiche sur Synergie Family");
         msg.setText("Bonjour,\n Merci de remplir vos informations sur le lien suivant :\n" +
                 url + "/activity-leader-edit/" + token);
-
         javaMailSender.send(msg);
 
     }
     public void sendInformationActivityLeader(ActivityLeader activityLeader,
-                                              String subject, String content){
+                                              String subject, String content) {
 
         SimpleMailMessage msg = new SimpleMailMessage();
         msg.setTo(activityLeader.getEmail());
         msg.setSubject(subject);
         msg.setText(content);
-
         javaMailSender.send(msg);
-
     }
 
-    public void sendActivityLeaderByFilter(ActivityLeader activityLeader){
+    public void sendActivityLeaderByFilter(ActivityLeader activityLeader) {
 
         SimpleMailMessage msg = new SimpleMailMessage();
         msg.setTo(activityLeader.getEmail());
         msg.setSubject("Information");
         msg.setText("Bonjour,\n Voici les informations pour votre prochaine mission : ");
-
         javaMailSender.send(msg);
-
     }
 }
